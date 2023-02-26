a18a3a237d5c1b45c7162ef2a3aa63752750ddcf
Armandase <armand.damiens31@gmail.com>
2023-02-26 10:48:16 +0100

    problem 3 fix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 pub mod problem35;
 pub mod problem1;
 pub mod problem2;
+pub mod problem3;
 pub mod problem4;
 
 fn main() {
     //println!("There is {} numbers", problem35::circular_primes());
     //println!("There is {} numbers", problem1::multiple_of_3_or_5());
    // println!("There is {} numbers", problem2::even_fibonacci_numbers());
-    println!("There is {} numbers", problem4::largest_palindrome_product());
+   // println!("There is {} numbers", problem4::largest_palindrome_product());
+    println!("There is {} numbers", problem3::largest_prime_factor());
 }
--- src/problem3.rs
+++ src/problem3.rs
@@ -1,4 +1,31 @@
-pub fn largest_prime_factor() -> i32{
-    let pb = 600851475143;
+fn sqrt(nb : u64) -> u64{
+    let mut index = 0;
+    while index < nb{
+        if index * index >= nb{
+            return index as u64;
+        }
+        index += 1;
+    }
+    return 0;
+}
 
+pub fn largest_prime_factor() -> i32{
+    let pb : u64 = 600851475143;
+    let mut factor = 2;
+    let mut nb = 0;
+    let tmp;
+    let max = sqrt(pb);
+   
+    while pb % factor != 0{
+        factor += 1;
+    }
+    tmp = factor;
+    while factor < max {
+        if pb % factor == 0{
+            nb = factor;
+        }
+        factor += 1;
+    }
+    nb /= tmp;
+    nb as i32
 }
